305165a6d947f35384b72bb13775da03731ce581
glaulher <glaulher.developer@gmail.com>
2022-01-15 13:53:48 -0300

    update class

---
+++ ChapterIII/rentx_aula/rentx/src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+export function Home() {
+  return (
+    <Container>
+      <Title>Open up App.tsx to start working on your app!</Title>
+    </Container>
+  );
+}
