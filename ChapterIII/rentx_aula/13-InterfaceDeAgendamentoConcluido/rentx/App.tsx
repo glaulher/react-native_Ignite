b1eb05707e99715de10e8aeade9323014ba9b207
glaulher <glaulher.developer@gmail.com>
2022-02-04 20:53:21 -0300

    update readme and class

---
+++ ChapterIII/rentx_aula/13-InterfaceDeAgendamentoConcluido/rentx/App.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components';
+
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from '@expo-google-fonts/inter';
+
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+} from '@expo-google-fonts/archivo';
+
+import { Home } from './src/screens/Home';
+import theme from './src/styles/theme';
+import { CarDetails } from './src/screens/CarDetails';
+import { Scheduling } from './src/screens/Scheduling';
+import { SchedulingDetails } from './src/screens/SchedulingDetails';
+import { SchedulingComplete } from './src/screens/SchedulingComplete';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+  });
+  if (!fontsLoaded) return <AppLoading />;
+
+  return (
+    <ThemeProvider theme={theme}>
+      {/* <CarDetails /> */}
+      <SchedulingComplete />
+    </ThemeProvider>
+  );
+}
